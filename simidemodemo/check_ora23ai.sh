d60ae74dcf2c89e9537ace27b433cd9dd18d2786
Kevin Lazarz <kevin.lazarz@oracle.com>
2025-08-07 05:55:08 +0200

    all updates

---
+++ simidemodemo/check_ora23ai.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if the `ora23ai` user can connect to the `sql` database
+until echo "exit" | sqlplus sh/<password>@23ai:1521/freepdb1; do
+  echo "Waiting for DB ready...."
+  sleep 30
+done
+
+
+echo "DB container is ready. Starting Python application..."
+exec python3.12 app.py  
